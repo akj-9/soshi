5d9d6a01f92cd0b62fc1d4e362fc50ff671d9fef
Aksh Jain <akshjain090@gmail.com>
2025-06-01 16:38:34 +0530

    Initial commit: setup Next.js, Tailwind, 3D layout

---
+++ src/3d/ProductPreview.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+
+export default function ProductPreview() {
+  return (
+    <div className="w-full h-96 bg-gray-100 rounded-2xl shadow-lg">
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <mesh rotation={[0, 0.5, 0]}>
+          <boxGeometry args={[2, 2, 2]} />
+          <meshStandardMaterial color="hotpink" />
+        </mesh>
+        <OrbitControls />
+      </Canvas>
+    </div>
+  )
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="w-full py-4 bg-gray-900 text-white text-center text-lg font-medium shadow">
+      FashionX | Modern Clothing for the Future
+    </nav>
+  )
+}
