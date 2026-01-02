c5afbbe4678c8c158699e7f0a2474e6300f0170f
Aksh Jain <akshjain090@gmail.com>
2026-01-02 23:58:10 +0530

    demo commit

--- src/3d/ProductPreview.tsx
+++ src/3d/ProductPreview.tsx
@@ -10,7 +10,7 @@ export default function ProductPreview() {
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         <mesh rotation={[0, 0.5, 0]}>
-          <boxGeometry args={[2, 2, 2]} />
+          <boxGeometry args={[2, 1, 2]} />
           <meshStandardMaterial color="hotpink" />
         </mesh>
         <OrbitControls />
